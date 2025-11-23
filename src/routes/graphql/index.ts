85f5da601a9376ec60c34b288c6fb3fcc023e355
SlavikusVOG <slavaolish@gmail.com>
2025-11-23 15:12:43 +0300

    feat: implement validator

--- src/routes/graphql/index.ts
+++ src/routes/graphql/index.ts
@@ -1,6 +1,28 @@
 import { FastifyPluginAsyncTypebox } from '@fastify/type-provider-typebox';
 import { createGqlResponseSchema, gqlResponseSchema } from './schemas.js';
-import { graphql } from 'graphql';
+import {
+  graphql,
+  GraphQLSchema,
+  GraphQLObjectType,
+  GraphQLString,
+  Source,
+  parse,
+  validate
+} from 'graphql';
+import { queryType } from './queries/query.js';
+import depthLimit from 'graphql-depth-limit';
+
+function validationErrors(query: string) {
+  const schema = new GraphQLSchema({
+    query: queryType
+  })
+  const source = new Source(query);
+  const ast = parse(source);
+  const rules = [
+    depthLimit(5),
+  ];
+  return validate(schema, ast, rules);
+}
 
 const plugin: FastifyPluginAsyncTypebox = async (fastify) => {
   const { prisma } = fastify;
